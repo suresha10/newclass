8ee4fbc975d2b47b676318eb4bf45a21feda15ec
suresh a <asureshdevops10@gmail.com>
2021-07-13 17:10:49 +0530

    project

---
+++ src/main/groovy/com/example/demo1/Demo1Application.groovy
@@ -0,0 +1,13 @@
+package com.example.demo1
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Demo1Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Demo1Application, args)
+	}
+
+}
